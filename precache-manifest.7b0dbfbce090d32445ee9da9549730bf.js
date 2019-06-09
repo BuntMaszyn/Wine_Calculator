676dc9ffa3ff5d849ffd416c6c783cbe05c3de9a
Rafal Baran <rafaubaran@gmail.com>
2019-06-09 10:20:15 +0200

    Updates

---
+++ precache-manifest.7b0dbfbce090d32445ee9da9549730bf.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "042532d19667978eb82fd4523f5938b2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "eb37ee5dac95924c617e",
+    "url": "/static/css/main.16981ced.chunk.css"
+  },
+  {
+    "revision": "21e0455ab157e3c726f6",
+    "url": "/static/js/2.4282bdd0.chunk.js"
+  },
+  {
+    "revision": "eb37ee5dac95924c617e",
+    "url": "/static/js/main.d3dbf5b7.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
