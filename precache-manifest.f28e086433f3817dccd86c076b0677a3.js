22b8a3d612d39778f1db6173e6157c60de5f2f2e
Rafal Baran <rafaubaran@gmail.com>
2019-06-09 10:26:31 +0200

    Updates

---
+++ precache-manifest.f28e086433f3817dccd86c076b0677a3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "24795911f4b723bc65c1a365921f0234",
+    "url": "/Wine_Calculator/index.html"
+  },
+  {
+    "revision": "b6437eee52e9ea4ba1e1",
+    "url": "/Wine_Calculator/static/css/main.16981ced.chunk.css"
+  },
+  {
+    "revision": "21e0455ab157e3c726f6",
+    "url": "/Wine_Calculator/static/js/2.4282bdd0.chunk.js"
+  },
+  {
+    "revision": "b6437eee52e9ea4ba1e1",
+    "url": "/Wine_Calculator/static/js/main.bb58c2c8.chunk.js"
+  },
+  {
+    "revision": "651cb5be47ca229c29fc",
+    "url": "/Wine_Calculator/static/js/runtime~main.13e2fced.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.7b0dbfbce090d32445ee9da9549730bf.js"
+  "/Wine_Calculator/precache-manifest.f28e086433f3817dccd86c076b0677a3.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Wine_Calculator/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
